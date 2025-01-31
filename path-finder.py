96f95761e94b9460d71665f066b492860c199067
Julio Leon <juliopena143@icloud.com>
2025-01-31 13:24:08 -0700

    printing maze

--- path-finder.py
+++ path-finder.py
@@ -20,15 +20,19 @@ def print_maze(maze, stdscr, path=[]):
     BLUE = curses.color_pair(1)
     RED = curses.color_pair(2)
 
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j, value)
+
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
-    # stdscr.clear()
-    # stdscr.addstr(0, 0, "hello world!", blue_and_black)
-    # stdscr.refresh()
-    # stdscr.getch()
+    stdscr.clear()
+    print_maze(maze, stdscr)
+    stdscr.refresh()
+    stdscr.getch()
 
 
 wrapper(main)
